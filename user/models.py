ca3dc03ebfb682017f0ed07b3ceea8f60f35b53c
liurongshuai <1140091878@qq.com>
2019-11-05 12:10:07 +0800

    加了性别隐藏

--- user/models.py
+++ user/models.py
@@ -10,6 +10,7 @@ class User(models.Model):
         ('male', '男性'),
         ('female', '女性'),
         ('no', '无性'),
+        ('hidden','隐藏'),
     )
 
     phonenum = models.CharField(max_length=20, unique=True, verbose_name='手机号')
